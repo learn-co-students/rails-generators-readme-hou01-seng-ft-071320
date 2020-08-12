e55fc3a2b96762a081e5bd1a6a2380fa5aee1b3e
dscottcole <67556122+dscottcole@users.noreply.github.com>
2020-08-12 12:06:06 -0500

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -12,8 +12,7 @@ class PostsController < ApplicationController
   end
 
   def create
-    @post = Post.new(params)
-    @post.save
+    @post = Post.create(params)
     redirect_to post_path(@post)
   end
 
